92d8bec2cca7bd5fbf833ddfd158776a956bff7e
DrAzrael <u8_aleszl_lbn@technischools.com>
2024-09-18 09:45:42 +0200

    nigga

---
+++ src/main/java/com/nigga/demo/NiggaApplication.java
@@ -0,0 +1,13 @@
+package com.nigga.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiggaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiggaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nigga/demo/NiggaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nigga.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiggaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
